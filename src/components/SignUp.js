5a138a40f7db196704786b28536253c99ade6ecb
AyushThakur34 <thakurayush9887@gmail.com>
2025-06-10 15:32:57 +0530

    added user section

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,9 +8,12 @@ const SignUp = ()=> {
     const [user, setUser] = useState("student");
     const mainHeading = "Join the millions learning to code with StudyNotion for free";
 
-    const handleUserState = ()=> {
-        if(user === "student") setUser("instructor");
-        else setUser("student");
+    const handleUserState = (event)=> {
+        event.preventDefault();
+        if(event.target.value != user) {
+                if(user === "student") setUser("instructor");
+                else setUser("student");
+        }
     }
     
     return (
@@ -19,8 +22,12 @@ const SignUp = ()=> {
                 <LandingPageHeading heading={mainHeading} size={"2xl"}/>
 
                 {/* user */}
-                <div>
-                    
+                <div className="text-white gap-x-1 flex mt-4 font-semibold
+                rounded-xl w-fit p-1">
+                    <button className={`rounded-xl py-1 px-4 ${user === "student" ? `bg-[#151d25]` : `bg-inherit`}`}
+                     value={"student"} onClick={handleUserState}>Student</button>
+                    <button className={`rounded-xl py-1 px-4 ${user === "instructor" ? `bg-[#151d25]` : `bg-inherit`}`}
+                     value={"instructor"} onClick={handleUserState}>Instructor</button>
                 </div>
 
                 <SignInWithGoogle/>
